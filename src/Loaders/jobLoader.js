5b5315fe9e717cdf7be2d3ca67eff5881d4e0b26
gaurabwebdev <gaurabbose40@gmail.com>
2023-04-11 18:40:30 +0600

    whitespace removed from json

--- src/Loaders/jobLoader.js
+++ src/Loaders/jobLoader.js
@@ -1,1 +1,8 @@
-export const loadedJobs = async () => {};
+export const loadedJobs = async () => {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/Porgramming-Hero-web-course/b7a9-career-hub-gaurabwebdev/main/public/allJobs.json?token=GHSAT0AAAAAAB66KS3S4JJ63UKTVRYCH4Y2ZBVMDQQ"
+  );
+  const jobs = await res.json();
+
+  return jobs.jobs;
+};
