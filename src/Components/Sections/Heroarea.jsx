ad2e4e919265bd8e2f42a9ab15dd561be62f1867
gaurabwebdev <gaurabbose40@gmail.com>
2023-04-11 17:12:55 +0600

    heroarea done

--- src/Components/Sections/Heroarea.jsx
+++ src/Components/Sections/Heroarea.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Heroarea = () => {
   return (
-    <div className="my-container flex justify-center items-center gap-5">
-      <div className="flex flex-col gap-3">
-        <h1 className="text-5xl font-semibold">
+    <div className="my-container flex flex-col lg:flex-row justify-center items-center gap-5">
+      <div className="flex flex-col gap-5 w-full lg:w-50">
+        <h1 className="text-5xl font-semibold tracking-wide leading-snug">
           One Step Closer To Your
           <br></br>
           <span className="active">Dream Job</span>
         </h1>
-        <p className="text-xl text-gray-500">
+        <p className="text-xl text-gray-500 tracking-wide leading-snug">
           Explore thousands of job opportunities with all the information you
           need. Its your future. Come find it. Manage all your job application
           from start to finish.
@@ -17,7 +17,11 @@ const Heroarea = () => {
         <button className="my-btn">Get Started</button>
       </div>
       <div>
-        <img src="https://ibb.co/PgZv3Dq" alt="hero-image" />
+        <img
+          className="w-full lg:w-50"
+          src="https://i.ibb.co/GRv4BHz/P3-OLGJ1-copy-1.png"
+          alt="hero-image"
+        />
       </div>
     </div>
   );
