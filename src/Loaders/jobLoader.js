45bda30c92d01be302b9d7cd874d92f8863e4069
gaurabwebdev <gaurabbose40@gmail.com>
2023-04-11 18:42:54 +0600

    get backed to as it is

--- src/Loaders/jobLoader.js
+++ src/Loaders/jobLoader.js
@@ -1,7 +1,5 @@
 export const loadedJobs = async () => {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/Porgramming-Hero-web-course/b7a9-career-hub-gaurabwebdev/main/public/allJobs.json?token=GHSAT0AAAAAAB66KS3S4JJ63UKTVRYCH4Y2ZBVMDQQ"
-  );
+  const res = await fetch("allJobs.json");
   const jobs = await res.json();
 
   return jobs.jobs;
