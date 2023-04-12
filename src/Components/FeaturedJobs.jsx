f988aaa0ac9a9fe5ef3b78c3b90ff7cc7de82526
gaurabwebdev <gaurabbose40@gmail.com>
2023-04-12 13:28:09 +0600

    see all button activated

--- src/Components/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs.jsx
@@ -5,19 +5,29 @@ import JobCard from "./JobCard";
 const FeaturedJobs = () => {
   const allJobs = useContext(JobContext);
   const [featuredJobs, setFeaturedJobs] = useContext(FeaturedJobContext);
+  const showAll = () => {
+    setFeaturedJobs(allJobs);
+  };
   return (
-    <div className="my-container">
-      <h2 className="text-4xl font-semibold text-center">Featured Jobs</h2>
-      <p className="text-xl text-gray-500 tracking-wide leading-snug text-center mt-3">
-        Explore thousands of job opportunities with all the information you
-        need. Its your future
-      </p>
-      <div className="grid grid-cols-1 lg:grid-cols-2 p-3 gap-2 my-5">
-        {featuredJobs.map((job) => (
-          <JobCard status={true} key={job.jobId} job={job}></JobCard>
-        ))}
+    <>
+      <div className="my-container">
+        <h2 className="text-4xl font-semibold text-center">Featured Jobs</h2>
+        <p className="text-xl text-gray-500 tracking-wide leading-snug text-center mt-3">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 p-3 gap-2 my-5">
+          {featuredJobs.map((job) => (
+            <JobCard status={true} key={job.jobId} job={job}></JobCard>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="my-continer flex justify-center mb-5">
+        <button onClick={showAll} className="my-btn">
+          See All Jobs
+        </button>
+      </div>
+    </>
   );
 };
 
